77707ad8d49e26c145cdbd696b58d657de94e134
dajing <gudajing@yeah.net>
2020-11-23 12:08:54 +0100

    Update

---
+++ TP1/Sujet-PID/BicycleToPointControl.m
@@ -0,0 +1,21 @@
+function [ u ] = BicycleToPointControl( xTrue,xGoal )
+%Computes a control to reach a pose for bicycle
+%   xTrue is the robot current pose : [ x y theta ]'
+%   xGoal is the goal point
+%   u is the control : [v phi]'
+
+
+% TODO
+
+    Krho = 15;
+    Kalpha = 10;
+
+    error = xGoal-xTrue;
+    goalDist = norm(error(1:2));
+    alpha = AngleWrap(atan2(error(2),error(1))-xTrue(3));
+    
+    u(1) = Krho * goalDist;
+    u(2) = Kalpha * alpha;
+    
+end
+
